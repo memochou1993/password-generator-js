55ff35129ce2df1c4d3ca58c4eaa142aefc6f139
Memo Chou <memochou1993@gmail.com>
2021-05-09 23:44:51 +0800

    Update main function

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,14 @@ interface Config {
 
 export default class Generator {
   static generate(config: Config): string {
-    const { length, letters, symbols, numbers } = config;
+    const { length } = config;
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    const numbers = '012345678901234567890123456789012345678901234567890123456789';
     let characters = '';
-    if (letters) characters += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (symbols) characters += '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-    if (numbers) characters += '0123456789';
+    if (config.letters) characters += letters;
+    if (config.symbols) characters += symbols;
+    if (config.numbers) characters += numbers;
     if (!characters) return '';
     const rdn = (n: number): number => Math.floor(Math.random() * n);
     const rds = (s: string): string => s[(rdn(s.length))];
